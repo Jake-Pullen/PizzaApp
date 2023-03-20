f845a1008e08e0df4613ea7f6d04b31d56c10e5f
Alex Dimmock <Alex.Dimmock@theaccessgroup.com>
2023-03-20 15:25:31 +0000

    initial setup

---
+++ PizzaDB/OrderStatus.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE dbo.OrderStatus (
+  orderstatus_id INT PRIMARY KEY,
+  orderstatus_date DATE NOT NULL,
+  orderstatus_time TIME NOT NULL,
+  orderstatus_status VARCHAR(20) NOT NULL
+  FOREIGN KEY (orderstatus_id) REFERENCES Orders(order_id)
+);
---
+++ PizzaDB/Payments.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE dbo.Payment (
+  payment_id INT PRIMARY KEY,
+  order_id INT NOT NULL,
+  payment_date DATE NOT NULL,
+  payment_time TIME NOT NULL,
+  payment_method VARCHAR(20) NOT NULL,
+  payment_amount DECIMAL(10,2) NOT NULL,
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+);
