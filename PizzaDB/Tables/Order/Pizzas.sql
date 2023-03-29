1bb2495a75d5b61acd0c703931fab3b81e35dd9c
Alex Dimmock <Alex.Dimmock@theaccessgroup.com>
2023-03-29 15:17:46 +0100

    Added FK

--- PizzaDB/Tables/Order/Pizzas.sql
+++ PizzaDB/Tables/Order/Pizzas.sql
@@ -2,8 +2,10 @@
   order_pizza_id INT NOT NULL,
   order_id INT NOT NULL,
   pizza_template_id INT NULL,
+  pizza_size_id INT NULL,
   is_custom BIT NOT NULL DEFAULT 0,
   CONSTRAINT pk_order_pizzas PRIMARY KEY (order_pizza_id),
   FOREIGN KEY (order_id) REFERENCES [order].[details](order_id),
-  FOREIGN KEY (pizza_template_id) REFERENCES [pizza].[templates](template_id)
+  FOREIGN KEY (pizza_template_id) REFERENCES [pizza].[templates](template_id),
+  FOREIGN KEY (pizza_size_id) REFERENCES [pizza].[sizes](size_id)
 );
