6f60e71b799c17a127cebfb9ff37950c92cd5edf
Alex Dimmock <Alex.Dimmock@theaccessgroup.com>
2023-03-29 15:44:22 +0100

    fixed status history FK

--- PizzaDB/Tables/Order/status_history.sql
+++ PizzaDB/Tables/Order/status_history.sql
@@ -3,5 +3,6 @@
   date_created DATETIME NOT NULL,
   status_id INT NOT NULL,
   CONSTRAINT pk_order_status_history PRIMARY KEY (order_status_id),
-  FOREIGN KEY (order_status_id) REFERENCES [order].[details](order_id)
+  FOREIGN KEY (order_status_id) REFERENCES [order].[details](order_id),
+  FOREIGN KEY (status_id) REFERENCES [order].[status](status_id)
 );
