41cc203367ac85c20162ae48e12f95fe37cc61d1
Alex Dimmock <Alex.Dimmock@theaccessgroup.com>
2023-03-29 15:41:24 +0100

    added order status history and order status

---
+++ PizzaDB/Tables/Order/status_history.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [order].[status_history] (
+  order_status_id INT NOT NULL,
+  date_created DATETIME NOT NULL,
+  status_id INT NOT NULL,
+  CONSTRAINT pk_order_status_history PRIMARY KEY (order_status_id),
+  FOREIGN KEY (order_status_id) REFERENCES [order].[details](order_id)
+);
--- PizzaDB/Tables/order/details.sql
+++ PizzaDB/Tables/order/details.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [order].[details] (
   order_id INT NOT NULL,
   customer_id INT NOT NULL,
-  order_date DATE NOT NULL,
-  order_total DECIMAL(10,2) NOT NULL,
+  created_date DATE NOT NULL,
+  total_cost DECIMAL(10,2) NOT NULL,
   CONSTRAINT pk_order_details PRIMARY KEY (order_id),
   FOREIGN KEY (customer_id) REFERENCES [customer].[details](customer_id)
 );
