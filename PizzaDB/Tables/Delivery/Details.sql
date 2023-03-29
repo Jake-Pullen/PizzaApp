04b7408d0d4e878c1c4ff0d9f780aa1fcc0b398a
Alex Dimmock <Alex.Dimmock@theaccessgroup.com>
2023-03-29 15:26:12 +0100

    added driver details and FK from delivery

--- PizzaDB/Tables/Delivery/Details.sql
+++ PizzaDB/Tables/Delivery/Details.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [delivery].[details] (
   order_id INT NOT NULL,
+  driver_id INT NOT NULL,
   recipient_name VARCHAR(255),
   address_line_1 VARCHAR(255),
   address_line_2 VARCHAR(255),
@@ -8,5 +9,6 @@
   postcode VARCHAR(10),
   country VARCHAR(255)
   CONSTRAINT pk_delivery_details PRIMARY KEY (order_id),
-  FOREIGN KEY (order_id) REFERENCES [order].[details](order_id)
+  FOREIGN KEY (order_id) REFERENCES [order].[details](order_id),
+  FOREIGN KEY (driver_id) REFERENCES [delivery].[drivers](driver_id)
 );
--- PizzaDB/Tables/Delivery/drivers.sql
+++ PizzaDB/Tables/Delivery/drivers.sql
@@ -1,4 +1,7 @@
 ﻿CREATE TABLE [delivery].[drivers]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+  driver_id INT NOT NULL,
+  driver_name VARCHAR(255) NOT NULL,
+  driver_phone VARCHAR(20) NOT NULL,
+  CONSTRAINT pk_delivery_drivers PRIMARY KEY (driver_id)
 )
