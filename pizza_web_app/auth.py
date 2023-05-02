85068daf30ee93936c26cf7befd5f250491e0a48
Jake Pullen <Jake.Pullen@theaccessgroup.com>
2023-05-02 13:00:47 +0100

    log on page mostly done

--- pizza_web_app/auth.py
+++ pizza_web_app/auth.py
@@ -1,36 +1,34 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 from pizza_web_app import bcrypt
-from db_read import db_user
+from db_read import get_user_id, get_user_info
 
 auth_bp = Blueprint('auth',__name__, template_folder='templates/auth')
 
 authentication_error = 'incorrect user name or password, please try again'
 
-def check_user_exists(user_name):
-    user_id = db_user.get_user_id(user_name)
-    if user_id:
-        flash(authentication_error,category='warning')
-        return False#render_template(url_for(auth.login))
-    else:
-        return user_id
-
-def check_password_match(user_name,password):
-    user_id = check_user_exists(user_name)
-    user_info = db_user.get_user_info(user_id)
-    stored_password = user_info.password_hash
-    password_check = bcrypt.check_password_hash(stored_password,password)
-    if password_check == True:
-        pass
-        #email and password has matched, log user in
-    else:
-        flash(authentication_error,category='warning')
-        return redirect(url_for('auth.log_in'))
-
 @auth_bp.route('/login', methods=["GET","POST"])
 def log_in():
     if request.method == "POST":
         #form filled and sent, go do the work
-        pass
+        data = request.form
+        user_name = data.get('email')
+        password = data.get('password')
+        user_id = get_user_id(user_name)
+        if not user_id:
+            #user does not exist
+            flash(authentication_error,category='warning')
+            return(render_template("log_in.html"))
+        user_info = get_user_info(user_id)
+        stored_password = user_info.password_hash
+        password_check = bcrypt.check_password_hash(stored_password,password)
+        if password_check == True:
+            pass
+            #email and password has matched, log user in
+            #give user_id session token(s)
+            #redirect to pizza page? 
+        else:
+            flash(authentication_error,category='warning')
+            return redirect(url_for('auth.log_in'))
     elif request.method == 'GET':
         #page hit, display log in form
         return(render_template("log_in.html"))
--- pizza_web_app/db_read.py
+++ pizza_web_app/db_read.py
@@ -1,25 +1,36 @@
 import pyodbc
+import socket
 
-db_connection_string = 'a_string_here'
+def sql_ver(host_name):
+    if host_name == 'Work-JP': #because JP is stupid enough to have 2 sql engines.... 
+        return 'MSSQLSERVER22'
+    else:
+        return 'MSSQLSERVER'
+host_name = socket.gethostname()
+sql_version = sql_ver(host_name)
+
+server = f'{host_name}\{sql_version}'
+database = 'pizza_master' #probably? maybe 'pizzaDB'
+db_connection_string = fr'Driver=SQL Server;Server={server};Database={database};Trusted_Connection=yes;'
+
 
 def connection():
-    cstr = db_connection_string.value
+    cstr = db_connection_string#.value
     conn = pyodbc.connect(cstr)
     return(conn)
 
-class db_user:
-    def get_user_id(user_name):
-        conn = connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT customer_id FROM customer.details WHERE email = ?",user_name)
-        user_id = cursor.fetchone()
-        conn.close()
-        return user_id
+def get_user_id(user_name):
+    conn = connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT customer_id FROM customer.details WHERE email = ?",user_name)
+    user_id = cursor.fetchone()
+    conn.close()
+    return user_id
 
-    def get_user_info(user_id):
-        conn = connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM customer.details WHERE email = ?",user_id)# i know * bad practice, fight me
-        user = cursor.fetchone()
-        conn.close()
-        return user
+def get_user_info(user_id):
+    conn = connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT customer_id, email, full_name FROM customer.details WHERE customer_id = ?",user_id)# i know * bad practice, fight me
+    user = cursor.fetchone()
+    conn.close()
+    return user
