d1d422c9fad911533b9dcb39a53fcbfbace78e7d
Alex Dimmock <Alex.Dimmock@theaccessgroup.com>
2023-05-05 16:43:14 +0100

    Added the python project to VS and fixed missing tables

--- PizzaDB/Tables/Customer/Details.sql
+++ PizzaDB/Tables/Customer/Details.sql
@@ -1,11 +1,11 @@
 ﻿CREATE TABLE [customer].[details] (
   customer_id INT NOT NULL IDENTITY(1,1),
-  email VARCHAR(128) NOT NULL,
-  full_name VARCHAR(100) NOT NULL,
-  phone_num VARCHAR(20) NOT NULL,
-  street_address VARCHAR(100) NOT NULL,
-  city VARCHAR(50) NOT NULL,
-  postcode VARCHAR(10) NOT NULL,
+  email VARCHAR(128) NULL,
+  full_name VARCHAR(100) NULL,
+  phone_num VARCHAR(20) NULL,
+  street_address VARCHAR(100) NULL,
+  city VARCHAR(50) NULL,
+  postcode VARCHAR(10) NULL,
   password_hash NVARCHAR(MAX) NULL
   CONSTRAINT pk_customer_details PRIMARY KEY (customer_id),
   CONSTRAINT uc_customer_email UNIQUE (email),
