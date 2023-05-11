762ea4e67dba19ff176588eee4917bf5288d2e51
Jake Pullen <Jake.Pullen@theaccessgroup.com>
2023-05-11 15:28:46 +0100

    updated db_read back to azure db credentials

--- PizzaWebApp/db_read.py
+++ PizzaWebApp/db_read.py
@@ -2,15 +2,15 @@ from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 import pyodbc
 
-import socket
-host_name = socket.gethostname()
-database = 'pizzaDB'
-db_connection_string = fr'Driver=SQL Server;Server={host_name};Database={database};Trusted_Connection=yes;'
+#import socket
+#host_name = socket.gethostname()
+#database = 'pizzaDB'
+#db_connection_string = fr'Driver=SQL Server;Server={host_name};Database={database};Trusted_Connection=yes;'
 
-#key_vault_url = 'https://adjp-pizza-co-key-vault.vault.azure.net/'
-#credential = DefaultAzureCredential()
-#client = SecretClient(vault_url=key_vault_url,credential=credential)
-#db_connection_string = client.get_secret('adjp-pizza-co-db-connection-string')
+key_vault_url = 'https://adjp-pizza-co-key-vault.vault.azure.net/'
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=key_vault_url,credential=credential)
+db_connection_string = client.get_secret('adjp-pizza-co-db-connection-string')
 
 
 def connection():
